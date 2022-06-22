4860ca31990cd50333652afd7f825446aaa6c71f
Dorothy Ewuah <dorothyewuah@gmail.com>
2022-06-22 23:58:22 +0000

    get bug fixed
    get bug fixed

--- frontend/src/app/messages/messages.component.ts
+++ frontend/src/app/messages/messages.component.ts
@@ -10,7 +10,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class MessagesComponent implements OnInit {
   response: any;
-  messages:any;
+  messages: any;
 
   constructor(
     public webService: WebService,
